e5f98bff76da06e0bbba28216d42afc6c598f522
Patricia Sanes <psanesl93@gmail.com>
2023-04-18 19:41:58 -0300

    Background image in Layout update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
+import Image from "next/image";
+import Background from "../public/assets/background.png";
 
 type Props = {
   children: ReactNode;
@@ -8,7 +10,15 @@ type Props = {
 const Layout: React.FC<Props> = (props) => (
   <div>
     <Header />
-    <div className="layout">{props.children}</div>
+    <div className="layout">
+      <Image
+        className="background"
+        src={Background}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="content">{props.children}</div>
+    </div>
     <style jsx global>{`
       html {
         box-sizing: border-box;
@@ -42,6 +52,17 @@ const Layout: React.FC<Props> = (props) => (
     <style jsx>{`
       .layout {
         padding: 0 2rem;
+      }
+      .content {
+        position: relative;
+        z-index: 0;
+      }
+
+      .background {
+        position: absolute;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
       }
     `}</style>
   </div>
